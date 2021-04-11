af679ee1d603f408bbfafb243bfdfad29df886c3
bargergo <bargergo@gmail.com>
2021-04-11 19:48:19 +0200

    About fragment

--- app/src/main/java/hu/bme/aut/tvshows/ui/about/AboutFragment.kt
+++ app/src/main/java/hu/bme/aut/tvshows/ui/about/AboutFragment.kt
@@ -1,4 +1,4 @@
-package hu.bme.aut.tvshows.ui.slideshow
+package hu.bme.aut.tvshows.ui.about
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,16 +8,16 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import hu.bme.aut.tvshows.R
 
-class SlideshowFragment : Fragment() {
+class AboutFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        textView.text = "This is slideshow Fragment"
+        val root = inflater.inflate(R.layout.fragment_about, container, false)
+        val textView: TextView = root.findViewById(R.id.text_about)
+        textView.text = "This is about Fragment"
         return root
     }
 }
