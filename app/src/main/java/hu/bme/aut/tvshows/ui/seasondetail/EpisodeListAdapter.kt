87ccfb8c9b53bfaab7695193a899c434842a8db5
bargergo <bargergo@gmail.com>
2021-05-01 18:03:50 +0200

    Can click on episodes

--- app/src/main/java/hu/bme/aut/tvshows/ui/seasondetail/EpisodeListAdapter.kt
+++ app/src/main/java/hu/bme/aut/tvshows/ui/seasondetail/EpisodeListAdapter.kt
@@ -1,13 +1,17 @@
 package hu.bme.aut.tvshows.ui.seasondetail
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import hu.bme.aut.tvshows.databinding.ListelementEpisodeBinding
 import hu.bme.aut.tvshows.model.Episode
 
 class EpisodeListAdapter(val fragment: Fragment, val episodes: List<Episode>) : RecyclerView.Adapter<EpisodeListAdapter.EpisodeViewHolder>() {
+
+    val context: Context = fragment.requireContext()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EpisodeViewHolder {
         return EpisodeViewHolder(
@@ -24,6 +28,10 @@ class EpisodeListAdapter(val fragment: Fragment, val episodes: List<Episode>) : 
 
         holder.binding.tvNumber.text = episode.number.toString()
         holder.binding.tvTitle.text = episode.name
+
+        holder.itemView.setOnClickListener {
+            Toast.makeText(context, "Episode ${episode.number} clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
