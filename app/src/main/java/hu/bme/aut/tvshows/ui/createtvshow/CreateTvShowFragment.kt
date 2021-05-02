9000d4c437594c339b61f0ade8af2f5910488083
bargergo <bargergo@gmail.com>
2021-05-02 19:47:44 +0200

    Added some validation

--- app/src/main/java/hu/bme/aut/tvshows/ui/createtvshow/CreateTvShowFragment.kt
+++ app/src/main/java/hu/bme/aut/tvshows/ui/createtvshow/CreateTvShowFragment.kt
@@ -1,6 +1,8 @@
 package hu.bme.aut.tvshows.ui.createtvshow
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -32,8 +34,33 @@ class CreateTvShowFragment : Fragment(), CreateTvShowContract.View {
     ): View {
         _binding = FragmentCreatetvshowBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        binding.etPremiered.addTextChangedListener(
+            object: TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    val datePattern = Regex("^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])\$")
+                    val isValid = if (s == null) false else datePattern.matches(s)
+                    if (!isValid)
+                        binding.etPremiered.error = "Type a date like 2021-01-23"
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                }
+
+            }
+        )
+
         binding.btnClickMe.setOnClickListener {
-            Toast.makeText(activity, binding.etTitle.text, Toast.LENGTH_SHORT).show()
+            if (!isValid())
+                showMessage("There are some validation errors!")
         }
         return view
     }
@@ -45,5 +72,13 @@ class CreateTvShowFragment : Fragment(), CreateTvShowContract.View {
     override fun onDestroy() {
         presenter.cleanup()
         super.onDestroy()
+    }
+
+    private fun isValid(): Boolean {
+        return binding.etGenres.error == null &&  (binding.etGenres.text?.length ?: 0) > 0
+                && binding.etImageUrl.error == null
+                && binding.etPremiered.error == null && (binding.etPremiered.text?.length ?: 0) > 0
+                && binding.etSummary.error == null && (binding.etSummary.text?.length ?: 0) > 0
+                && binding.etTitle.error == null && (binding.etTitle.text?.length ?: 0) > 0
     }
 }
